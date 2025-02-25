097344cd6f7df74bece0fe3a423b9ff511c04d28
Sonia <adonisbudo@gmail.com>
2025-02-24 21:06:03 -0500

    I changed sfjawio

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and Welcome!");
+        System.out.print("Yp!");
 
         }
     }
